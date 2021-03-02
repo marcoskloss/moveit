af89972c394171648d13a224549882d689a441ef
marcoskloss <marcoskloss2001@gmail.com>
2021-03-02 09:55:35 -0300

    add page title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,28 @@
 import { LoginBox } from '../components/LoginBox'
+import Head from 'next/head'
 
 import styles from '../styles/pages/Home.module.css'
 
 export default function Home() {
 
   return (
-    <div className={styles.container}>
-      <img src="/background-image.png" />
-      <section>
-        <img src="/logo-full.svg" alt="Move it" />
+    <>
+      <Head>
+        <title>Entrar | move.it</title>
+      </Head>
+      <div className={styles.container}>
+        <img src="/background-image.png" />
+        <section>
+          <img src="/logo-full.svg" alt="Move it" />
 
-        <h1>Bem-vindo</h1>
-        <div>
-          <img src="/icons/Github.svg" alt="Github" />
-          <span>Entre com seu Github para começar.</span>
-        </div>
-        <LoginBox />
-      </section>
-    </div>
+          <h1>Bem-vindo</h1>
+          <div>
+            <img src="/icons/Github.svg" alt="Github" />
+            <span>Entre com seu Github para começar.</span>
+          </div>
+          <LoginBox />
+        </section>
+      </div>
+    </>
   )
 }
